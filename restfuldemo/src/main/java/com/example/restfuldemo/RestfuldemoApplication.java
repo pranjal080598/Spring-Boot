3463af28c4ac6ab8ec49637519d26c36d38e390c
pranjal080598 <pranjal080598@gmail.com>
2020-07-14 20:32:53 +0530

    Restful Template

--- restfuldemo/src/main/java/com/example/restfuldemo/RestfuldemoApplication.java
+++ restfuldemo/src/main/java/com/example/restfuldemo/RestfuldemoApplication.java
@@ -2,6 +2,8 @@ package com.example.restfuldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestfuldemoApplication {
@@ -9,5 +11,9 @@ public class RestfuldemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfuldemoApplication.class, args);
 	}
+	@Bean
+	   public RestTemplate getRestTemplate() {
+	      return new RestTemplate();
+	   }
 
 }
