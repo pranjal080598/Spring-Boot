4a787db8674487905edb32f7b418588286018b31
pranjal080598 <pranjal080598@gmail.com>
2020-07-15 19:02:27 +0530

    CORS

--- thymeleaf/src/main/java/com/example/thymeleaf/ThymeleafApplication.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/ThymeleafApplication.java
@@ -1,6 +1,12 @@
 package com.example.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,5 +15,13 @@ public class ThymeleafApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafApplication.class, args);
 	}
-
+	@Bean
+	   public WebMvcConfigurer corsConfigurer() {
+	      return new WebMvcConfigurerAdapter() {
+	         @Override
+	         public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/products").allowedOrigins("http://localhost:8080");
+	         }
+	      };
+	   }
 }
