c755f4d503ae7d75e48b8c062b393830ec015cf5
pranjal080598 <pranjal080598@gmail.com>
2020-07-15 15:17:53 +0530

    File Upload and Download

---
+++ fileoperation/src/test/java/com/example/fileoperation/FileoperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileoperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
